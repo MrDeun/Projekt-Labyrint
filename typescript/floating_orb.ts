b05d853f4db855821bddba4fd2e08cac3da41335
MrDeun <pmichalak16@gmail.com>
2023-12-26 02:15:53 +0100

    Finished campfire - need to add more lights around the wood

--- typescript/floating_orb.ts
+++ typescript/floating_orb.ts
@@ -1,24 +1,42 @@
 import * as THREE from 'three'
 
 const loader = new THREE.TextureLoader();
-const mirror = new THREE.MeshStandardMaterial({emissive:0xffffff,emissiveIntensity:0.6,emissiveMap:loader.load("/8k_sun.jpg"),metalness:0.0,roughness:1.0,map:loader.load("/8k_sun.jpg")});
-const geometry = new THREE.SphereGeometry(1.5,32,16,0,Math.PI*2,0,Math.PI);
 
-const brick_geometry = new THREE.BoxGeometry(0.6,0.5,1.5,1,1,1);
+const brick_geometry = new THREE.BoxGeometry(1.9,1.8,5.4,1,1,1);
 const brick_material = new THREE.MeshStandardMaterial({color:0xcb4154});
 
-const log_geometrty = new THREE.BoxGeometry(1.0,1.0,2.0,1,1,1);
-const log_material = new THREE.MeshStandardMaterial({color:0x5d4632}); 
+const log_offest = 3.0;
+const log_thickness = 0.8;
+const log_geometrty = new THREE.BoxGeometry(log_thickness,log_thickness,9.0,1,1,1);
+const log_material = new THREE.MeshStandardMaterial({color:0x211911,emissiveMap:loader.load('/8k_sun.png'),emissive:0xe25822});
 
 const log = new THREE.Mesh(log_geometrty,log_material);
 
+function log_generate_level()
+{
+    let logGroup = new THREE.Group();
+    for (let index = 0; index < 2; index++) 
+    {
+        let temp = new THREE.Mesh(log_geometrty,log_material);
+        temp.position.y = -6.25 + log_thickness;
+
+        if( index%2 == 0)
+            {temp.position.x += log_offest;}
+        else
+            {temp.position.x -= log_offest;}
+
+        logGroup.add(temp);
+    }
+    return logGroup;
+}
+
+
 export class Orb
 {
-    mesh: THREE.Mesh;
     all: THREE.Group;
     light: THREE.PointLight;
 
-    static radius = 3.0;
+    static radius = 8.0;
 
     constructor()
     {
@@ -27,7 +45,7 @@ export class Orb
         for (let index = 0; index < 8; index++) 
         {
             let temp = new THREE.Mesh(brick_geometry,brick_material);
-            temp.position.y = -6.25 + 0.6;
+            temp.position.y = -6.25 + 0.4;
             temp.position.x = Orb.radius * Math.cos( index * Math.PI/4 );
             temp.position.z = Orb.radius * Math.sin( index * Math.PI/4 );
             temp.rotation.y -= index * Math.PI/4;
@@ -35,43 +53,39 @@ export class Orb
             brickArray.push(temp);
         }
 
-        let logArray = [];
 
-        for (let index = 0; index < 2; index++) 
+        let stack = [];
+
+        for (let index = 0; index < 6; index++) 
         {
-            let temp = new THREE.Mesh(log_geometrty,log_material);
-            temp.position.y = -6.25 + 1.0;
-
-            if( index%2 == 0)
-                {temp.position.z += 0.7;}
-            else
-                {temp.position.z -= 0.7;}
-
-                logArray.push( temp );
-            
+            let temp = log_generate_level();
+            temp.position.y += index * log_thickness;
+            if( index%2 == 1 )
+                {
+                    temp.rotation.y += Math.PI/2;
+                }
+            stack.push(temp);    
         }
 
-        this.mesh = new THREE.Mesh(geometry,mirror);
         this.light = new THREE.PointLight(0xe25822,128.0,150,2.0);
-        this.mesh.add(this.light);
         this.all = new THREE.Group();
-        this.all.add(this.mesh);
+        this.all.add(this.light);
         for (let index = 0; index < brickArray.length; index++) 
             {
                 this.all.add(brickArray[index]);
             }
-        for (let index = 0; index < logArray.length; index++) 
+
+            for (let index = 0; index < stack.length; index++) 
             {
-                this.all.add(logArray[index]);
+                this.all.add(stack[index]);
             }
+
     }
         
 
     updatePosition()
     {
-        this.mesh.position.y = 3 * Math.sin( Date.now() * Math.PI/2400);
-        this.mesh.rotation.y += 3 * Math.PI/120;
-        this.light.position.y = 3 * Math.sin( Date.now() * Math.PI/2400);
+        this.light.position.y = 2.0 * Math.sin( Date.now() * Math.PI/1800 );
         this.light.rotation.y += 3 * Math.PI/120;
     }
 }
